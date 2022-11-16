3064bf46938666035bee89d6833a9aab7a67fc93
dedane <evanskeema@gmail.com>
2022-11-16 14:07:37 +0300

    add-hero-section

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -10,7 +10,8 @@ function Home() {
                 fontSize: '45px', 
                 fontStyle: 'normal',
                 fontWeight: '400', 
-                letterSpacing: '-.02em'}} variant='h2'>Rent a <span style={{color: '#A02279'}} >place</span> away <br></br> from  <span style={{color: '#A02279'}}>Home</span> in the <span style={{color: '#A02279'}}>Metaverse</span></Typography>
+                letterSpacing: '-.02em',
+                width: 'fit-content'}} variant='h2'>Rent a <span style={{color: '#A02279'}} >place</span> away <br></br> from  <span style={{color: '#A02279'}}>Home</span> in the <span style={{color: '#A02279'}}>Metaverse</span></Typography>
       </Container>
     </div>
   )
