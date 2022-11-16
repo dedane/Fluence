41c810576ad600f992d3135f3d7e4a4513617da3
dedane <evanskeema@gmail.com>
2022-11-16 14:14:15 +0300

    textfield-and-button

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,4 +1,5 @@
-import { Typography,Container } from '@mui/material'
+import { Typography,Container,TextField,Button } from '@mui/material'
+import { Box } from '@mui/system'
 import React from 'react'
 
 function Home() {
@@ -6,12 +7,25 @@ function Home() {
     <div>
       
       <Container direction='row'>
-        <Typography sx={{ display: 'block', 
+        <Box mt={10}>
+              <Typography sx={{ display: 'block', 
                 fontSize: '45px', 
                 fontStyle: 'normal',
                 fontWeight: '400', 
                 letterSpacing: '-.02em',
                 width: 'fit-content'}} variant='h2'>Rent a <span style={{color: '#A02279'}} >place</span> away <br></br> from  <span style={{color: '#A02279'}}>Home</span> in the <span style={{color: '#A02279'}}>Metaverse</span></Typography>
+        </Box>
+        <Box mt={4}> 
+                  <Typography variant='h6' display='block'>
+                    we provide you access to luxury and affordable
+                   houses in the <br></br> metaverse, get a chance to turn 
+                   your <br></br>imagination to reality at your comfort zone
+                  </Typography>
+                  <Box mt={5} mb={5}>
+                  <TextField sx={{width: '500'}}  label="search for location" />
+                  <Button mt={10} sx={{ backgroundColor: '#A02279', width: '230px', height: '54px' }} variant="contained">Search</Button>
+                  </Box>
+        </Box>
       </Container>
     </div>
   )
