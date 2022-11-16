6df211c5bb0aa8dff1599b2374f06fd70f3abac8
dedane <evanskeema@gmail.com>
2022-11-16 12:33:52 +0300

    footer-done

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -15,44 +15,58 @@ function Footer() {
           px: 2,
           mt: 'auto'}}>
       <Container >
-        <Grid container spacing={5} columns={16}>
-          <Grid item xs={12} sm={3}>
+        <Grid container spacing={10} direction="row"
+  justifyContent="center"
+  alignItems="center">
+          <Grid item xs={8} sm={3} >
+            <Box mr={10}>
             <Box>
             <img src='https://ik.imagekit.io/ugyodiq15/Group_w2l_ITd4T.png?ik-sdk-version=javascript-1.4.3&updatedAt=1668567639604' alt='logo' />
             </Box>
-            <Box mt={15}>
-            <Stack direction="row" spacing={8}>
-              <FaFacebookF fontSize="30"/>
-              <FaInstagram fontSize="40" />
-              <FaTwitter fontSize="40"/>
+            <Box mt={10}>
+            <Stack direction="row" spacing={5}>
+              <FaFacebookF />
+              <FaInstagram  />
+              <FaTwitter />
             </Stack>
             </Box>
-            <Box mt={10}>
+            </Box>
+            <Box mt={5}>
               <Typography> <span>&#169;</span> 2022 Metabnb</Typography>
             </Box>
           </Grid>
-          <Grid item xs={12} sm={3}>
+          <Grid item xs={8} sm={3} f>
             <Box>
-              <img src='' />
+              <Typography variant="h5">Community</Typography>
             </Box>
-            <Box>
+            <Box mt={8}>
+              <Typography sx={{fontWeight: '400', fontSize: '14px', lineHeight: '35x'}}>NFT</Typography>
+              <Typography sx={{fontWeight: '400', fontSize: '14px', lineHeight: '35x'}}>Tokens</Typography>
+              <Typography sx={{fontWeight: '400', fontSize: '14px', lineHeight: '35x'}}>Landlords</Typography>
+              <Typography sx={{fontWeight: '400', fontSize: '14px', lineHeight: '35x'}}>Discord</Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={3}>
+          <Box>
+              <Typography variant="h5">Places</Typography>
+            </Box>
+            <Box mt={8}>
+              <Typography sx={{fontWeight: '400', fontSize: '14px', lineHeight: '35x'}}>Castle</Typography>
+              <Typography sx={{fontWeight: '400', fontSize: '14px', lineHeight: '35x'}}>Farms</Typography>
+              <Typography sx={{fontWeight: '400', fontSize: '14px', lineHeight: '35x'}}>Beach</Typography>
+              <Typography sx={{fontWeight: '400', fontSize: '14px', lineHeight: '35x'}}>Learn More</Typography>
               
             </Box>
           </Grid>
           <Grid item xs={12} sm={3}>
           <Box>
-              <img src='' />
+              <Typography variant="h5">About us</Typography>
             </Box>
-            <Box>
-              
-            </Box>
-          </Grid>
-          <Grid item xs={12} sm={3}>
-          <Box>
-              <img src='' />
-            </Box>
-            <Box>
-              
+            <Box mt={8}>
+            <Typography sx={{fontWeight: '400', fontSize: '14px', lineHeight: '35x'}}>Road map</Typography>
+              <Typography sx={{fontWeight: '400', fontSize: '14px', lineHeight: '35x'}}>Creators</Typography>
+              <Typography sx={{fontWeight: '400', fontSize: '14px', lineHeight: '35x'}}>Career</Typography>
+              <Typography sx={{fontWeight: '400', fontSize: '14px', lineHeight: '35x'}}>Contact Us</Typography>
             </Box>
           </Grid>
 
