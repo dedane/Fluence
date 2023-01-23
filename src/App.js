e78b5d65ba2440af662e25d5802f02edcce8c014
dedane <evanskeema@gmail.com>
2023-01-23 06:28:04 +0300

    updated-main-js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Navbar from './Components/Navbar'
 import Footer from './Components/Footer'
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Routes, Route } from "react-router-dom";
-import Home from './Pages/Home'
-import Place from './Pages/Place'
+import Home from '../src/Pages/Home'
+import Place from '../src/Pages/Place'
 
 function App() {
   const theme = createTheme({
