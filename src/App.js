7ba009f30eac016ac1bfd550aacf70de8638ee0b
dedane <evanskeema@gmail.com>
2023-01-28 05:52:31 +0300

    added-switch

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from './Components/Navbar'
 import Footer from './Components/Footer'
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Switch,  } from "react-router-dom";
 import Home from '../src/Pages/Home'
 import Place from '../src/Pages/Place'
 
@@ -18,12 +18,14 @@ function App() {
   return (
     <ThemeProvider theme={theme} >
       <Navbar />
+      <Switch>
     <Routes>
       
       <Route path='/' element={<Home />} ></Route>
       <Route path='/place' element={<Place />}></Route>
       
     </Routes>
+    </Switch>
     <Footer />
      
     </ThemeProvider>
