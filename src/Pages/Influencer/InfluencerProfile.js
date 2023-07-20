564ff34dbc2c71e4497bf3f60f54b0b177b15d99
dedane <evanskeema@gmail.com>
2023-07-20 06:04:52 +0300

    profile-image-section

--- src/Pages/Influencer/InfluencerProfile.js
+++ src/Pages/Influencer/InfluencerProfile.js
@@ -1,8 +1,38 @@
+import { Box,Container,Typography,Grid, ListItemText, List, ListItemIcon, ListItemButton } from '@mui/material'
+import { FiMail } from "react-icons/fi";
 import React from 'react'
 
 function InfluencerProfile() {
   return (
-    <div>InfluencerProfile</div>
+    <Box>
+        <Container>
+            <Typography variant='h3'>Update Your Profile To meet new brands</Typography>
+            <Typography>Fill in the fields bellow</Typography>
+
+            <Box mt={10}>
+                <Grid container direction='row'>
+                    <Grid item>
+                        <img src="https://ik.imagekit.io/ugyodiq15/Rectangle_48_Q76SuQ79k.png?updatedAt=1689821615944" alt='profile-image'/>
+                    </Grid>
+                    <Grid item>
+                        <Box ml={8}>
+                        <Typography variant='h5'>John Kamau</Typography>
+                        </Box>
+                        <List>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                 <FiMail />
+                                </ListItemIcon>
+                                <ListItemText>
+                                    johnkamau@gmail.com
+                                </ListItemText>
+                            </ListItemButton>
+                        </List>
+                    </Grid>
+                </Grid>
+            </Box>
+        </Container>
+    </Box>
   )
 }
 
