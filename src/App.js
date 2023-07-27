8f808733619ab960cd242534309a06880fc76a89
dedane <evanskeema@gmail.com>
2023-07-27 15:09:30 +0300

    Brand-pages

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ import SignIn from '../src/Pages/SignIn'
 import SignUp from '../src/Pages/SignUp'
 import BrandSignIn from '../src/Pages/BrandSignIn'
 import BrandSignUp from '../src/Pages/BrandSignUp'
+import PickInfluencers from '../src/Pages/Brand/PickInfluencers'
+import Payments from '../src/Pages/Brand/Payments';
+import CampaignForm from './Pages/Brand/CampaignForm';
+import BrandLanding from './Pages/Brand/BrandLanding';
 import InfluencerLanding from './Pages/Influencer/InfluencerLanding';
 import InfluencerSocials from './Pages/Influencer/InfluencerSocials';
 import InfluencerProfile from './Pages/Influencer/InfluencerProfile';
@@ -33,6 +37,10 @@ function App() {
       <Route path='/SignUp' element={<SignUp />}></Route>
       <Route path='/BrandSignUp' element={<BrandSignUp />}></Route>
       <Route path='/BrandSignIn' element={<BrandSignIn />}></Route>
+      <Route path='Brand/BrandLanding' element={<BrandLanding />}></Route>
+      <Route path='Brand/Payment' element={<Payments />}></Route>
+      <Route path='Brand/CampaignForm' element={<CampaignForm />}></Route>
+      <Route path='Brand/PickInfluencers' element={<PickInfluencers />}></Route>
       <Route path='Influencer/InfluencerLanding' element={<InfluencerLanding />}></Route>
       <Route path='Influencer/InfluencerSocials' element={<InfluencerSocials />}></Route>
       <Route path='Influencer/InfluencerProfile' element={<InfluencerProfile />}></Route>
---
+++ src/Pages/Brand/BrandLanding.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BrandLanding() {
+  return (
+    <div>BrandLanding</div>
+  )
+}
+
+export default BrandLanding
